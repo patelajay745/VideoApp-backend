521d3dcfa9be340fba60f4c9212168c2edc79867
Ajay patel <patel.ajay745@gmail.com>
2024-09-03 23:19:28 -0400

    added middleware and standard api error and response

--- src/index.js
+++ src/index.js
@@ -4,4 +4,10 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: "./.env" });
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port : ${process.env.PORT} `);
+        });
+    })
+    .catch();
