2f048637ba74c6ca1c121a3c6e489699f7fdf35a
Ajay patel <patel.ajay745@gmail.com>
2024-09-06 08:20:15 -0400

    added getUser,updatePassword,updateAvtar

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -200,4 +200,67 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     }
 });
 
-export { registerUser, loginUser, logoutuser, refreshAccessToken };
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+    const { oldPassword, newPassword } = req.body;
+    const user = await User.findById(req.user?.id);
+
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid old password");
+    }
+
+    user.password = newPassword;
+
+    await user.save({ validateBeforeSave: false });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Password is changed"));
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, req.user, "current user fetched successfully")
+        );
+});
+
+const updateUserAvatar = asyncHandler(async (req, res) => {
+    const avtarLocalPath = req.files?.avatar?.[0]?.path;
+
+    if (!avtarLocalPath) {
+        throw new ApiError(400, "avatar file is missing");
+    }
+
+    const avatar = await uploadOnCloudinary(avtarLocalPath);
+
+    if (!avatar) {
+        throw new ApiError(400, "Something wrong in uploading file");
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+        { _id: req.user._id },
+        {
+            $set: {
+                avatar: avatar.url,
+            },
+        },
+        { new: true }
+    ).select("-password -refreshToken");
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, updatedUser, "Avtar is updated"));
+});
+
+export {
+    registerUser,
+    loginUser,
+    logoutuser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateUserAvatar,
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,9 +4,13 @@ import {
     registerUser,
     logoutuser,
     refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateUserAvatar,
 } from "../controllers/user.js";
 import { upload } from "../middlewares/multer.js";
 import { verifyJWT } from "../middlewares/auth.js";
+import multer from "multer";
 
 const router = Router();
 
@@ -25,5 +29,16 @@ router.post("/login", loginUser);
 router.post("/logout", verifyJWT, logoutuser);
 
 router.post("/refresh-token", refreshAccessToken);
+
+router.post("/change-password", verifyJWT, changeCurrentPassword);
+
+router.get("/user", verifyJWT, getCurrentUser);
+
+router.post(
+    "/updateUserAvatar",
+    verifyJWT,
+    upload.fields([{ name: "avatar", maxCount: 1 }]),
+    updateUserAvatar
+);
 
 export default router;
